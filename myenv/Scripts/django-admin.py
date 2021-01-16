16408d7672952950c5ca9d2660a8391e7042f274
Abdul <58429982+Abdul-213@users.noreply.github.com>
2021-01-16 22:18:53 +0000

    setup django site

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abdul\work\personal projects\marco-polo\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
